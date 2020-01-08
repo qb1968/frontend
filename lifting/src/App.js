089925e5bdd062eeee68266460b3ccd9362f46b5
jarrodskahill <jarrodskahill@gmail.com>
2020-01-07 16:04:49 -0800

    small changes

--- lifting/src/App.js
+++ lifting/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import "./styles/Trackerpage.scss"
-
-
 import LoginForm from './components/Login/loginForm';
-
 import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom"
 import AddExercise from './components/addExercise';
 import MyWorkouts from './components/myExercises';
 import NavBar from './components/NavBar';
+import PrivateRoute from './components/PrivateRoute';
 
 
 
@@ -15,16 +13,11 @@ function App() {
   return (
     <Router>
     <div className="App">
-
-      <Router>
-            <Route exact path="/"><h1 className="title">Weight Lifting Tracker</h1></Route>
-            <Route  path="/login"><LoginForm/></Route>
-      </Router>
-
       <NavBar/>
       <section className="exercises">
-      <Switch>
-        <Route path="/" component={MyWorkouts} />
+      <Switch>            
+        <Route  path="/" component={LoginForm}/>
+        {/* <PrivateRoute path="/" component={MyWorkouts} /> */}
       </Switch>
       </section>
 
